0d414fa4a017063c0685c7ed00285d024ef530da
Hebin-aot <115780406+Hebin-aot@users.noreply.github.com>
2022-10-26 00:02:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ function createTask(){
                 <div class="checkbox-title d-flex align-items-center gap-3 ">
                     <input class="checkBox form-check-input rounded-circle " type="checkbox" value="" id=${i} onclick="statusChecking(this.id)">
                     <div>
-                        <span class="title-of-task m-0">${dataArray[i].textobj}  <img src="/images/Ellipse 1.png" alt=""></span><br>
+                        <span class="title-of-task m-0">${dataArray[i].textobj}  <img src="images/Ellipse 1.png" alt=""></span><br>
                         <span class="descrip d-none">${dataArray[i].descriptionobj}</span>
                         <span class="date-time m-0 d-flex align-items-center gap-2 pt-2"><img src="/images/Vector (5).png" alt=""> By ${dataArray[i].dateobj}</span>
                     </div>
